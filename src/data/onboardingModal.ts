adfade9c13d5dcd425c656e760f67a7e22f6c4f9
Cauã Pereira <cauapereira_@hotmail.com>
2026-01-16 20:44:21 -0300

    add onboarding modal component

---
+++ src/data/onboardingModal.ts
@@ -0,0 +1,20 @@
+export const onboardingSteps = [
+  {
+    title: "Create your first task",
+    description: `Start by creating a task you want to focus on
+`,
+    img: "/onboarding/first_pomodolist_step.png",
+    imgWidth: 300,
+    imgHeight: 100,
+  },
+  {
+    title: "Start your first PomoDoList",
+    description: `Each Pomodoro session is linked to a task, so you always know what you’re working on.
+
+    Start one by clicking on the tomato icon
+      `,
+    img: "/onboarding/second_pomodolist_step.png",
+    imgWidth: 600,
+    imgHeight: 80,
+  },
+];
---
+++ src/data/pomoDoListInfoSteps.ts
@@ -0,0 +1,55 @@
+export const steps = [
+  {
+    title: "What's PomoDoList?",
+    description: `PomoDoList is a productivity app that combines task management with the Pomodoro technique and Lofi Music to help you stay focused and get things done.
+       
+      Instead of juggling multiple tools, you organize your tasks and focus on them one session at a time — with clarity and intention.`,
+  },
+  {
+    title: "How Pomodoro improves focus?",
+    description: `The Pomodoro technique breaks work into short, focused sessions, followed by intentional breaks.
+
+      This approach helps you:
+
+        - Avoid mental fatigue
+
+        - Reduce procrastination
+
+        - Stay engaged for longer periods
+
+        You work with your brain, not against it.
+      `,
+  },
+  {
+    title: "Tasks + Pomodoro = Real Progress",
+    description: `
+        Working without a clear task leads to wasted time.
+
+        With PomoDoList, every Pomodoro session is tied to a specific task, so you always know:
+
+        - What you’re working on
+
+        - Why it matters
+
+        - When it's done
+
+        Focus becomes measurable progress.
+      `,
+  },
+  {
+    title: "Lofi Music for Flow State",
+    description: `PomoDoList includes optional lofi music to help you enter a flow state.
+      
+      The music is:
+
+      - Calm and non-distracting
+
+      - Fully independent from the timer
+
+      - Easy to control or disable
+      
+
+      Use it when it helps. Turn it off when it doesn’t.
+      `,
+  },
+];
