0d2ad694ada32a6f98a08423f5b6e3b01755766e
Cauã Pereira <cauapereira_@hotmail.com>
2026-01-18 23:12:38 -0300

    add new tracks

--- src/data/lofiTracks.ts
+++ src/data/lofiTracks.ts
@@ -1,8 +1,8 @@
 export const lofiTracks = [
   {
-    id: "lofi-1",
-    title: "Midnight Coffee",
-    src: "audio/lofi1.mp3",
+    id: "lofi-4",
+    title: "Rainy Lofi City",
+    src: "audio/lofi4.mp3",
   },
   {
     id: "lofi-2",
@@ -15,8 +15,28 @@ export const lofiTracks = [
     src: "audio/lofi3.mp3",
   },
   {
-    id: "lofi-4",
-    title: "Rainy Lofi City",
-    src: "audio/lofi4.mp3",
+    id: "lofi-1",
+    title: "Midnight Coffee",
+    src: "audio/lofi1.mp3",
+  },
+  {
+    id: "lofi-5",
+    title: "Relaxing Afternoon",
+    src: "audio/lofi5.mp3",
+  },
+  {
+    id: "lofi-6",
+    title: "Cold Night",
+    src: "audio/lofi6.mp3",
+  },
+  {
+    id: "lofi-7",
+    title: "Last train before get home",
+    src: "audio/lofi7.mp3",
+  },
+  {
+    id: "lofi-8",
+    title: "Echoes of what we were",
+    src: "audio/lofi8.mp3",
   },
 ];
