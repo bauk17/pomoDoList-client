d66cc4b4d901b68b9b96d28cba9838566c9090be
Cauã Pereira <cauapereira_@hotmail.com>
2026-02-11 17:38:53 -0300

    feat: add long break system

--- src/hooks/usePomodoro.ts
+++ src/hooks/usePomodoro.ts
@@ -3,9 +3,12 @@ import type { PomodoroState } from "../types/Pomodoro";
 
 const FOCUS_TIME = 25 * 60;
 const BREAK_TIME = 5 * 60;
+const LONG_BREAK_TIME = 15 * 60;
+const POMODOROS_BEFORE_LONG_BREAK = 4;
 
 export function usePomodoro(taskTitle: string) {
   const [pomodoro, setPomodoro] = useState<PomodoroState | null>(null);
+  const [, setCompletedPomodoros] = useState(0);
 
   function start() {
     setPomodoro({
@@ -38,10 +41,34 @@ export function usePomodoro(taskTitle: string) {
         if (prev.remainingSeconds <= 1) {
           const isFocus = prev.mode === "focus";
 
+          if (isFocus) {
+            setCompletedPomodoros((count) => {
+              const nextCount = count + 1;
+
+              const isLongBreak = nextCount % POMODOROS_BEFORE_LONG_BREAK === 0;
+
+              setPomodoro(
+                (current) =>
+                  current && {
+                    ...current,
+                    mode: isLongBreak ? "longbreak" : "break",
+                    remainingSeconds: isLongBreak
+                      ? LONG_BREAK_TIME
+                      : BREAK_TIME,
+                    status: "running",
+                  },
+              );
+
+              return nextCount;
+            });
+
+            return prev;
+          }
+
           return {
             ...prev,
-            mode: isFocus ? "break" : "focus",
-            remainingSeconds: isFocus ? BREAK_TIME : FOCUS_TIME,
+            mode: "focus",
+            remainingSeconds: FOCUS_TIME,
             status: "running",
           };
         }
@@ -65,13 +92,15 @@ export function usePomodoro(taskTitle: string) {
     const seconds = pomodoro.remainingSeconds % 60;
 
     const formatted = `${String(minutes).padStart(2, "0")}:${String(
-      seconds
+      seconds,
     ).padStart(2, "0")}`;
 
     document.title =
       pomodoro.mode === "focus"
         ? `PomoDoList - ${formatted} – Focus ⏱️`
-        : `PomoDoList - ${formatted} – Break ☕`;
+        : pomodoro.mode === "longbreak"
+          ? `PomoDoList - ${formatted} – Long Break 🛌`
+          : `PomoDoList - ${formatted} – Break ☕`;
 
     return () => {
       document.title = "PomoDoList";
